7b1027cdee2010b72b33e6302db3aba2ced5e3ad
iuventa <gachechiladzesal@gmail.com>
2026-05-22 11:55:09 +0400

    update aminals

--- Drum/js/scripts.js
+++
@@ -1,8 +0,0 @@
-let animals = document.querySelectorAll(".animal");
-
-for(let i = 0; i < animals.length; i++){
-    console.log(animals[i]);
-    animals[i].addEventListener("click", function(){
-        alert("damaklikes");
-    })
-}
